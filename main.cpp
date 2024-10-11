d0f59333c2ed539eb167349d0fb660caae14e982
Mellow-ss <kenthcomedero12515@gmail.com>
2024-10-11 20:47:06 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Kenth louie C. Comedero" << endl;
+    cout << "Prk. Nursery, Brgy. Maibo" << endl;
+    cout << "Aclc college of marbel" << endl;
+    cout << "BSIT-1" << endl;
+    return 0;
+}
